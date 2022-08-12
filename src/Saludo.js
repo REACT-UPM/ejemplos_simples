65ee2f2a3979f520ffa0e473448f82c5250f5321
Enrique <enrique.barra@upm.es>
2022-08-12 16:38:05 +0100

    primer ejemplo

---
+++ src/Saludo.js
@@ -0,0 +1,18 @@
+export default function Saludo(props) {
+  const elem = <h1>Esto es un titular</h1>;
+  const nombre = "Enrique Barra";
+  const usuario = { nombre: "Enrique", apellido: "Barra", edad: "40" };
+
+  function formatName(user) {
+    return user.nombre + " " + user.apellido + " de " + user.edad + " años";
+  }
+
+  return (
+    <div>
+      {elem}
+      <p>Hola {nombre}</p>
+      <p>Hola {formatName(usuario)}</p>
+    </div>
+  );
+}
+
