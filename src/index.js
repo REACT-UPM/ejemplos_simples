375568b53fdabd002d6b526ecde7c733eb495c08
Enrique <enrique.barra@upm.es>
2022-12-01 13:01:57 +0100

    new example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import User from './props_state/User';
+import User from './props_state2/User';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <User />);
