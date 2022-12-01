c43f8444862f1deb661eff0c2a2d0a517f5b63b5
Enrique <enrique.barra@upm.es>
2022-12-01 11:44:26 +0100

    small fix

--- src/composicion/App.js
+++ src/composicion/App.js
@@ -1,9 +1,13 @@
-import Comment from "./CommentWithPropTypes";
+import Comment from "./CommentFull";
 
 //Los meses en new Date empiezan en 0, por eso el 11 es diciembre
 const comment = {
   date: new Date(2025, 3, 11),
-  text: 'Me encanta aprender React!'
+  text: 'Me encanta aprender React!',
+  author: {
+    name: 'Enrique Barra',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
 };
 
 function App() {
